7ba46fccbf466d5a2953415ac6d2fe4514f2e607
Norlant1 <jobenbarrera@gmail.com>
2023-02-15 17:17:19 +0800

    fix-update-s3-when-new-content-builder-is-added

--- pages/admin/content-manager/[entity_type_slug]/[id].js
+++ pages/admin/content-manager/[entity_type_slug]/[id].js
@@ -120,7 +120,7 @@ export default function Type({ cache }) {
 
   const getS3Keys = (files) => {
     const fileKeys = Object.keys(files);
-    const s3Keys = fileKeys.map((file) => state.values[file].key);
+    const s3Keys = fileKeys.filter((file) => state.values[file].key);
     
     return s3Keys;
   };
@@ -142,7 +142,7 @@ export default function Type({ cache }) {
             entity_type_slug,
             entity_id: id,
           };
-
+          
           const formattedEntries = convertToFormData(entry);
 
           const response = await slsFetch(`/api/${entity_type_slug}/${id}`, {
